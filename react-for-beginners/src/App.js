591534026afd846d10ef0c4baf81bfb4f409c19e
blockchainnomad <blockchainnomad@gmail.com>
2023-05-30 23:25:54 +0900

    learn State

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  let [counter, setCounter] = useState(0);
+  const onClick = () => {
+    // setCounter(counter + 1);
+    // setCounter((current) => current + 1);
+    setCounter(function (current) {
+      return current + 1;
+    });
+  };
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h3>Total clicks: {counter}</h3>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
-
 export default App;
