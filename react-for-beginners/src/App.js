957be12a5335dea9af0a0ab0383223a1a9d290d5
blockchainnomad <blockchainnomad@gmail.com>
2023-06-05 21:05:15 +0700

    20230605

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,23 +1,28 @@
 import { useState, useEffect } from "react";
 
-function MinutesToHours() {}
-function KmToMiles() {
-  return <h3>KM 2 M</h3>;
+function Btn({ text, changeValue }) {
+  return (
+    <button
+      onClick={changeValue}
+      style={{
+        backgroundColor: "tomato",
+        color: "white",
+        padding: "10px 20px",
+        border: 0,
+        borderRadius: 10,
+      }}
+    >
+      {text}
+    </button>
+  );
 }
 function App() {
-  const [index, setIndex] = useState(0);
-  const onSelect = (event) => {
-    setIndex(event.target.value);
-  };
+  const [value, setValue] = useState("Save Changes");
+  const changeValue = () => setValue("Revert Changes");
   return (
     <div>
-      <h1>Super Converter</h1>
-      <select value={index} onChange={onSelect}>
-        <option value="0">Minutes & Hours</option>
-        <option value="1">Km & Miles</option>
-      </select>
-      {index === "0" ? <MinutesToHours /> : null}
-      {index === "1" ? <KmToMiles /> : null}
+      <Btn text={value} changeValue={changeValue} />
+      <Btn text="Continue" />
     </div>
   );
 }
