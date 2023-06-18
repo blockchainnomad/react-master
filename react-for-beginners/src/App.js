40960d69cea9f83359fe3eb57d6f2576a0faf019
blockchainnomad <blockchainnomad@gmail.com>
2023-06-19 00:08:25 +0900

    Update App.js
    movie app part one

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,14 +3,39 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.2&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => setMovies(json.data.movies));
+    getMovies();
   }, []);
   console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 export default App;
